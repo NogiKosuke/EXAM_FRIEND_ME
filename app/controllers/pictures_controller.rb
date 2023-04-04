50dd5847903570f5b85dcddf0577c518c3c4d92c
nogi kosuke <kskng0902@gmail.com>
2023-04-04 21:42:12 +0900

    投稿機能の実装

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,24 @@
+class PicturesController < ApplicationController
+  def index
+    @pictures = Picture.all
+  end
+  def new
+    @picture = Picture.new 
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+    redirect_to pictures_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image,:content)
+  end
+
+end
