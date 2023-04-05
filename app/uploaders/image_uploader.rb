04caf939462ca4c04a0f6a9d2cb3435d3f078556
nogi kosuke <kskng0902@gmail.com>
2023-04-05 14:03:10 +0900

    gitignoreの追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   storage :file
   
-  process resize_to_limit: [400, 600]
+  process resize_to_limit: [500, 700]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
